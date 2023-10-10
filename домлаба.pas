9f27f8cff4c2ab4b08ae924d8765cf915f76f365
Исупов Максим Олегович <144238218+maksincheks@users.noreply.github.com>
2023-10-10 08:30:04 +0300

    Add files via upload

---
+++ домлаба.pas
@@ -0,0 +1,23 @@
+﻿Program DomLaba01;
+var x,y:real;
+begin
+  Writeln ('Введите x');
+  readln (x); 
+  if x < -9 then begin
+    writeln ('Выражение при значение x = ', x:0:2, ' не имеет смысла');
+    end
+    else
+       if (x>= -9) and (x<0) then begin
+    writeln ('Выражение при значение x = ', x:0:2, ' не имеет смысла');
+    end
+    else
+      if (x>= 0) and (x<4) then begin
+        y := -x * tan(x) - power(x,3);
+        writeln ('Значение выражения y = ',y:2:2,' , при x = ' ,x:2:2);;
+      end
+     else
+       if (x>=4) then begin
+        y := (power (x,3) - 85);
+        writeln ('Значение выражения y = ',y:2:2,' , при x = ' ,x:2:2);;
+      end;
+end.
---
+++ домлаба02.pas
@@ -0,0 +1,25 @@
+﻿Program DomLaba02;
+var x,y:real;
+begin
+ x := -11;
+ repeat
+    if x < -9 then begin
+    writeln ('Выражение при значение x = ', x:0:2, ' не имеет смысла');
+    end
+    else
+       if (x>= -9) and (x<0) then begin
+    writeln ('Выражение при значение x = ', x:0:2, ' не имеет смысла');
+    end
+    else
+      if (x>= 0) and (x<4) then begin
+        y := -x * tan(x) - power(x,3);
+        writeln ('Значение выражения y = ',y:2:2,' , при x = ' ,x:2:2);;
+      end
+     else
+       if (x>=4) then begin
+        y := (power (x,3) - 85);
+        writeln ('Значение выражения y = ',y:2:2,' , при x = ' ,x:2:2);;
+      end;
+      x := x + 0.1;
+ until x>6;
+end.
